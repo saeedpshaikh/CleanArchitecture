1daeaae40ad07a10c6b569eb896b2d983050bf5f
saeedpshaikh <saeedshaikhz19@gmail.com>
2023-09-13 16:15:30 +0530

    Navigation issue fix

--- app/src/main/java/com/rbs/democleanarchitecture/MainActivity.kt
+++ app/src/main/java/com/rbs/democleanarchitecture/MainActivity.kt
@@ -114,32 +114,60 @@ fun BottomNavigationBar(
             val selected = item.route == backStackEntry.value?.destination?.route
             BottomNavigationItem(
                 selected = selected,
-                onClick = { onItemClick(item) },
+                onClick = { /*onItemClick(item) */
+
+                    navController.navigate(item.route) {
+                        // Pop up to the start destination of the graph to
+                        // avoid building up a large stack of destinations
+                        // on the back stack as users select items
+                        navController.graph.startDestinationRoute?.let { route ->
+                            popUpTo(route) {
+                                saveState = true
+                            }
+                        }
+                        // Avoid multiple copies of the same destination when
+                        // reselecting the same item
+                        launchSingleTop = true
+                        // Restore state when reselecting a previously selected item
+                        restoreState = true
+                    } },
                 selectedContentColor = Color.Green,
                 unselectedContentColor = Color.Gray,
-                icon = {
-                    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+
+                icon = { Icon(item.icon,contentDescription = item.name) },
+                label = { Text(item.name)},
+               /* icon = {
 
 
                         Icon(
                             imageVector = item.icon,
                             contentDescription = item.name
                         )
-                    }
+
                     Icon(
                         imageVector = item.icon,
                         contentDescription = item.name
                     )
 
                     if (selected) {
+
+                        Icon(
+                            imageVector = item.icon,
+                            contentDescription = item.name
+                        )
                         Text(
                             text = item.name,
                             textAlign = TextAlign.Center,
                             fontSize = 10.sp
                         )
+                       *//* Text(
+                            text = item.name,
+                            textAlign = TextAlign.Center,
+                            fontSize = 10.sp
+                        )*//*
                     }
 
-                }
+                }*/
             )
         }
     }
